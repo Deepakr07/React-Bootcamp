d0840da2b6213b4f65064e93da2d490ecab1d4c5
deepak <deepakrajesh567@gmail.com>
2024-08-19 10:23:35 +0530

    Created custom react components and rendered them

--- Day -01/First-React-Practice/index.js
+++ Day -01/First-React-Practice/index.js
@@ -1,3 +1,24 @@
-//using ReactDOM package to render jsx
+//1. using ReactDOM package to render jsx
 
-ReactDOM.render(<h1>I'm just trying out react</h1>,document.querySelector("#root"));
+//ReactDOM.render(<h1>I'm just trying out react</h1>,document.querySelector("#root"));
+
+//2.Composibility in react -- making reusable components
+
+function MainTitle(){
+    return(
+        <h1>Hello This is a sample Component</h1>
+    )
+}
+
+function MainContent(){
+    return(
+        <p>This is a dummy text to test out the component</p>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <MainTitle />
+        <MainContent />
+    </div>
+,document.querySelector("#root"));
