c84e5de8d17ee5f08ea8e2223ed454951b1a5bbc
deepak <deepakrajesh567@gmail.com>
2024-08-19 11:01:10 +0530

    worked on with jsx features and tried out alternate ways of rendering content

--- Day -01/First-React-Practice/index.js
+++ Day -01/First-React-Practice/index.js
@@ -1,24 +1,42 @@
 //1. using ReactDOM package to render jsx
 
-//ReactDOM.render(<h1>I'm just trying out react</h1>,document.querySelector("#root"));
+
+    //ReactDOM.render(<h1>I'm just trying out react</h1>,document.querySelector("#root"));
 
 //2.Composibility in react -- making reusable components
 
-function MainTitle(){
-    return(
-        <h1>Hello This is a sample Component</h1>
-    )
-}
+    // function MainTitle(){
+    //     return(
+    //         <h1>Hello This is a sample Component</h1>
+    //     )
+    // }
 
-function MainContent(){
-    return(
-        <p>This is a dummy text to test out the MainContent component</p>
-    )
-}
+    // function MainContent(){
+    //     return(
+    //         <p>This is a dummy text to test out the MainContent component</p>
+    //     )
+    // }
 
-ReactDOM.render(
+    // ReactDOM.render(
+    //     <div>
+    //         <MainTitle />
+    //         <MainContent />
+    //     </div>
+    // ,document.querySelector("#root"));
+
+
+//3.Working with jsx    
+    //what react is returning in jsx is plain old javaScript objects
+
+const pageContent = (
     <div>
-        <MainTitle />
-        <MainContent />
+        <h1>This is a JSX element</h1>
+        <p>This text is a part of the main jsx element</p>
     </div>
-,document.querySelector("#root"));
+)
+console.log(pageContent);
+
+const root = document.querySelector("#root");
+
+console.log(root);
+ReactDOM.render(pageContent,root);
