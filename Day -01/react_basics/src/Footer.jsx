5f32a046502e8c5455d161c2a553abc38ae28fa4
deepak <deepakrajesh567@gmail.com>
2024-08-24 23:36:08 +0530

    Organized components

---
+++ Day -01/react_basics/src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./challenge2.css"
+
+
+
+function Footer(){
+  return(
+    <footer><p>© 2024 Deepak development. All rights reserved.</p></footer>
+  )
+}
+
+export default Footer;
--- Day -01/react_basics/src/challenge2.jsx
+++ Day -01/react_basics/src/challenge2.jsx
@@ -1,45 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./challenge2.css"
-function Header(){
-  return(
-    <header>
-      <nav className="nav">
-        <img src = "./react-logo.png"  className="logo" / >
-        <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-      </nav>
-   </header>
-  )
-}
+import Header from "./Header";
+import Footer from "./Footer";
+import MainContent from "./MainContent";
 
 
-function Page() {
-    return (
-        <div>            
-            <h1>Reasons why I am excited to learn React</h1>
-            <ol>
-                <li>It's a popular library, so I'll be 
-                 able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                if I know React</li>
-            </ol>
-        </div>
-    )
-}
-
-function Footer(){
-  return(
-    <footer><p>© 2024 Deepak development. All rights reserved.</p></footer>
-  )
-}
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-rootElement.render(<div>
+rootElement.render(<div className="hero">
   <Header />
-  <Page />
+  <MainContent />
   <Footer/>
 </div>);
