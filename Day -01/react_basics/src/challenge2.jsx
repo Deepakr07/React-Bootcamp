1672ec38b0b6c68aa4cf292c7d6a4b383f7000bf
deepak <deepakrajesh567@gmail.com>
2024-08-24 23:19:02 +0530

    Style: added to nav

--- Day -01/react_basics/src/challenge2.jsx
+++ Day -01/react_basics/src/challenge2.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-
+import "./challenge2.css"
 function Header(){
   return(
     <header>
-      <nav>
-        <img src = "./react-logo.png" width = "40px"/>
-        <ul>
+      <nav className="nav">
+        <img src = "./react-logo.png"  className="logo" width="40px"/ >
+        <ul className="nav-items">
                     <li>Pricing</li>
                     <li>About</li>
                     <li>Contact</li>
