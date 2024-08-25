4c994e292b3a495d0f7275f430fa6ef9ac53b173
deepak <deepakrajesh567@gmail.com>
2024-08-25 19:21:13 +0530

    started:MainContent

---
+++ Day -01/react_basics/src/project-1/MainContent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function MainComponent(){
+  return(
+    <div className="main">
+      <h1>Fun Facts About React</h1>
+      <ul className="fact-list">
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
