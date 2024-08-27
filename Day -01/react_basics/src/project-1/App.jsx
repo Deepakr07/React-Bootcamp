8b1459794c2f23eb919a3a199acfe644bc98acbf
deepak <deepakrajesh567@gmail.com>
2024-08-27 22:08:49 +0530

    Added maincomponent

--- Day -01/react_basics/src/project-1/App.jsx
+++ Day -01/react_basics/src/project-1/App.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom"
 import "./style.css"
 import Header from "./Header";
+import MainComponent from "./MainContent";
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-rootElement.render(<Header />);
+rootElement.render(<>
+  <Header />
+  <MainComponent />
+
+</>);
--- Day -01/react_basics/src/project-1/MainContent.jsx
+++ Day -01/react_basics/src/project-1/MainContent.jsx
@@ -5,11 +5,17 @@ function MainComponent(){
   return(
     <div className="main">
       <h1>Fun Facts About React</h1>
-      <ul className="fact-list">
-        <li></li>
-        <li></li>
-        <li></li>
+     <div className="container">
+     <ul className="fact-list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
+      <img src="./reactjs-icon 2.png" alt="react-logo" />
+     </div>
     </div>
   )
 }
+export default MainComponent
