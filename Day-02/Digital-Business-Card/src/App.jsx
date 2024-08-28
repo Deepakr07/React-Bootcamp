17564d0ad20c639c837578398ab28e10c1cb2b59
deepak <deepakrajesh567@gmail.com>
2024-08-28 12:59:54 +0530

    completed : About component

--- Day-02/Digital-Business-Card/src/App.jsx
+++ Day-02/Digital-Business-Card/src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ProfileInfo from "./Components/ProfileInfo/ProfileInfo";
+import About from "./Components/About/About";
+import "./app.css"
 
 const rootELement = ReactDOM.createRoot(document.getElementById("root"));
-rootELement.render(
-  <ProfileInfo />
+rootELement.render(<div className="container">
+<ProfileInfo />
+<About />
+</div>
+  
 )
---
+++ Day-02/Digital-Business-Card/src/Components/About/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./about.css";
+
+function About(){
+  return(
+    <div className="main">
+      <h1>About</h1>
+      <p className="about">I'm a B.Tech graduate in Information Technology, currently working as a Software Engineer at TOA Software in Japan. With a passion for technology, I am actively sharpening my skills in frontend development.</p>
+      <h1>Interests</h1>
+      <p className="about">Sports Enthusiast, Gaming, Music, Travelling</p>
+    </div>
+    
+  )
+}
+
+export default About
