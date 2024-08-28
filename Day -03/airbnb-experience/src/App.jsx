8c1c9281ef22ca47d44c10dea576db1da450acf7
deepak <deepakrajesh567@gmail.com>
2024-08-28 23:22:52 +0530

    airbnb Hero section done

--- Day -03/airbnb-experience/src/App.jsx
+++ Day -03/airbnb-experience/src/App.jsx
@@ -1,9 +1,11 @@
-import Nav from "./Components/Nav/Nav"
+import Nav from "./Components/Nav/Nav";
+import Hero from "./Components/Hero/Hero";
 import "./App.css"
 const App = ()=>{
   return(
     <div className="container">
-      <Nav/>
+      <Nav />
+      <Hero />
     </div>
   )
 }
---
+++ Day -03/airbnb-experience/src/Components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import HeroImage from"../../../Assets/hero-img.png";
+import "./Hero.css";
+const Hero = ()=>{
+  return(
+    <div className="hero">
+      <img src={HeroImage} alt="hero image" className="hero-image"/>
+      <h1>Enjoy the Magic of Airbnb Experiences From the Comfort of Your Home</h1>
+      <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.From cooking classes with renowned chefs to virtual tours of stunning destinations, our curated experiences bring excitement directly to you. Connect with passionate experts and like-minded participants without ever leaving your living room. Discover new skills, cultures, and adventures in the comfort of your home. Explore a diverse range of activities and make unforgettable memories. Your next great experience is just a click away.</p>
+    </div>
+  )
+}
+export default Hero
