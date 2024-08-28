bc75da0eabfbf15cf0ac8ba79f537780f1eb1cd7
deepak <deepakrajesh567@gmail.com>
2024-08-28 21:38:56 +0530

    Updated : content in about section

--- Day-02/Digital-Business-Card/src/Components/About/About.jsx
+++ Day-02/Digital-Business-Card/src/Components/About/About.jsx
@@ -5,7 +5,7 @@ function About(){
   return(
     <div className="main">
       <h1>About</h1>
-      <p className="about">I'm a B.Tech graduate in Information Technology, currently working as a Software Engineer at TOA Software in Japan. With a passion for technology, I am actively sharpening my skills in frontend development.</p>
+      <p className="about">I'm a B.Tech graduate in Information Technology, and an upcoming Software Engineer at TOA Software in Japan. With a passion for technology, I am actively sharpening my skills in frontend development.</p>
       <h1>Interests</h1>
       <p className="about">Sports Enthusiast, Gaming, Music, Travelling</p>
     </div>
