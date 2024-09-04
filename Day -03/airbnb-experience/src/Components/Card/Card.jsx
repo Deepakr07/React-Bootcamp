2b21bd3901b5abbe1fd83c630db7ff8332c17031
deepak <deepakrajesh567@gmail.com>
2024-09-04 20:45:21 +0530

    fixed:double scrollbar issue

--- Day -03/airbnb-experience/src/Components/Card/Card.jsx
+++ Day -03/airbnb-experience/src/Components/Card/Card.jsx
@@ -2,17 +2,15 @@ import CardStar from "../../../Assets/star.png";
 import "./Card.css"
 const Card = (props)=>{
   return(
-    <div className="card-container">
-      <div className="card">
-        <div className="card-img"><img src={props.img} alt="card-image" /></div>
-        <div className="card-content">
-          <div className="row-1">
-            <img src={CardStar} alt="cardstar" />
-            <p className="rating">{props.rating} <span className="country-code"> ({props.reviewCount}).{props.countryName}</span></p>
-          </div>
-          <div className="row-2"><p>{props.cardName}</p></div>
-          <div className="row-3"><p><strong>From ${props.price}</strong> /Person</p></div>
+    <div className="card">
+      <div className="card-img"><img src={props.img} alt="card-image" /></div>
+      <div className="card-content">
+        <div className="row-1">
+          <img src={CardStar} alt="cardstar" />
+          <p className="rating">{props.rating} <span className="country-code"> ({props.reviewCount}).{props.countryName}</span></p>
         </div>
+        <div className="row-2"><p>{props.cardName}</p></div>
+        <div className="row-3"><p><strong>From ${props.price}</strong> /Person</p></div>
       </div>
     </div>
   )
