de720d8b33b564a2204277fbb7b8f4fcdacdbcd3
deepak <deepakrajesh567@gmail.com>
2024-09-04 19:24:42 +0530

    rendered card details array and mapped to card component

--- Day -03/airbnb-experience/src/App.jsx
+++ Day -03/airbnb-experience/src/App.jsx
@@ -1,20 +1,23 @@
 import Nav from "./Components/Nav/Nav";
 import Hero from "./Components/Hero/Hero";
 import Card from "./Components/Card/Card";
+import cardDetails from "./Components/CardDetails/CardDetails";
 import "./App.css"
 const App = ()=>{
+  const cardElements = cardDetails.map(details=>{
+    return <Card img = {details.img}
+                 rating = {details.rating}
+                 countryCode = {details.countryCode}
+                 countryName = {details.countryName}
+                 cardName = {details.cardName}
+                 price = {details.price}  
+    />
+  })
   return(
     <div className="container">
       <Nav />
       <Hero />
-      <Card 
-        img = "../Assets/card-img1.png"
-        rating = "5"
-        countryCode = "6"
-        courntryName = "USA"
-        cardName = "Life Lessons with Katie Zeferes"
-        price = "$138"
-      />
+      {cardElements}
     </div>
   )
 }
---
+++ Day -03/airbnb-experience/src/Components/CardDetails/CardDetails.jsx
@@ -0,0 +1,13 @@
+
+const cardDetails = [
+  {
+    img : "../Assets/card-img1.png",
+    rating : "5",
+    countryCode : "6",
+    countryName : "USA",
+    cardName : "Life Lessons with Katie Zeferes",
+    price : "$138"
+  }
+]
+
+export default cardDetails
